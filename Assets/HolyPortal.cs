0c2ae3ddf7f78ccffb3a95a63e74dc0f2b599573
heathen29 <diversant329@gmail.com>
2020-12-28 00:45:09 +0200

    1. Added enemy script to enable HP and ability to kill them. 2. Updated enemies prefabs with enemy script. 3. Added HolyPortal prefab. 4. Added HolyPortal script to hurt enemy and displace portal. 5. Rewrite player movement class, added jump.

---
+++ Assets/HolyPortal.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HolyPortal : MonoBehaviour
+{
+    [SerializeField] private int _damage;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Enemy"))
+        {
+            var enemy = other.GetComponent<MyEnemy>();
+            enemy.Hurt(_damage);
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            Destroy(gameObject);
+        }
+    }
+}

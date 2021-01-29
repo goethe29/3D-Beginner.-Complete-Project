b5dc3163b0ef9e5140d159ce8f92f754765e6113
heathen29 <diversant329@gmail.com>
2021-01-29 02:42:13 +0200

    Lesson 6. Added Gun Script.

---
+++ Assets/Scripts/Gun.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+    Vector3 _position;
+    [SerializeField] GameObject _boolet;
+    [SerializeField] private float _shootForce = 10f;
+    private Camera cam;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            _position = gameObject.transform.position;
+            
+            //var target = cam.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 5.3f));
+            //var targetDirection = target - _position; --- to shot at mouse position
+
+            var targetDirection = gameObject.transform.forward; //to shot forward
+
+            var boolet = Instantiate(_boolet, _position, Quaternion.identity );
+            var booletBody = boolet.GetComponent<Rigidbody>();
+
+            booletBody.AddForce(targetDirection * _shootForce); 
+        }
+    }
+}
